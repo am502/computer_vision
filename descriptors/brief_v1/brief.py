75fef34d757fd2e9a10ec76f4f11cdf2af69afad
am502 <antegzeperob@gmail.com>
2020-04-25 03:30:56 +0300

    add brief v1

--- descriptors/brief_v1/brief.py
+++ descriptors/brief_v1/brief.py
@@ -1,7 +1,8 @@
 import cv2
+import numpy as np
 
-from detectors.oriented_fast.oriented_fast import OrientedFast
 from descriptors.brief_v1.pair_generator import PairGenerator
+from detectors.oriented_fast.oriented_fast import OrientedFast
 
 
 class Brief:
@@ -12,6 +13,12 @@ class Brief:
         self.s = s
         self.s_half = int(s / 2)
         self.n = n
+        self.__init_rotation_matrices()
+
+    def __init_rotation_matrices(self):
+        self.sector = 12
+        alpha = [i for i in range(0, self.sector * 31, self.sector)]
+        self.matrices = [cv2.getRotationMatrix2D(center=(0, 0), angle=a, scale=1) for a in alpha]
 
     def compute(self):
         descriptors = []
@@ -19,10 +26,21 @@ class Brief:
         for c in self.corners:
             if self.__check_pixel_border(c.i, c.j):
                 pos1, pos2 = PairGenerator.g1(self.s_half, self.n)
+
+                index = round(c.angle / self.sector)
+                rotation_matrix = self.matrices[index]
+
+                new_pos1 = np.stack([pos1[:, 1], pos1[:, 0], np.zeros(len(pos1))], axis=1)
+                new_pos1 = np.round(np.dot(new_pos1, rotation_matrix.T))
+                new_pos1 = new_pos1.astype(int)
+                new_pos2 = np.stack([pos2[:, 1], pos2[:, 0], np.zeros(len(pos2), dtype=int)], axis=1)
+                new_pos2 = np.round(np.dot(new_pos2, rotation_matrix.T))
+                new_pos2 = new_pos2.astype(int)
+
                 descriptor = 0
                 for k in range(self.n):
-                    binary_test = 1 if self.image[c.i + pos1[k][0]][c.j + pos1[k][1]] < \
-                                       self.image[c.i + pos2[k][0]][c.j + pos2[k][1]] else 0
+                    binary_test = 1 if self.image[c.i + new_pos1[k][0]][c.j + new_pos1[k][1]] < \
+                                       self.image[c.i + new_pos2[k][0]][c.j + new_pos2[k][1]] else 0
                     descriptor += 2 ** k * binary_test
                 descriptors.append(descriptor)
                 corners.append(c)
@@ -36,12 +54,8 @@ def main():
     image = cv2.imread('../../resources/eagle.jpeg')
     grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    corners = OrientedFast(grayscale_image, 3, 60, 3).detect()
+    corners = OrientedFast(grayscale_image, 3, 60, 3, "x").detect()
     descriptors, c = Brief(grayscale_image, corners, 31, 256).compute()
-
-    print(len(corners))
-    for d in descriptors:
-        print(bin(d))
 
 
 if __name__ == "__main__":
--- trash/trash.py
+++ trash/trash.py
@@ -16,8 +16,23 @@ for k in range(n):
     d = d * 10 + bt
     dd += 2 ** k * bt
 
+print(pos1)
+print(pos2)
+print(pos1[:, 1])
 print(descriptor)
 print(d)
 print(dd, bin(dd))
 # print(pos1)
 # print(pos2)
+
+sector = 12
+alpha = [i for i in range(0, sector * 31, sector)]
+matrices = [cv2.getRotationMatrix2D(center=(0, 0), angle=a, scale=1) for a in alpha]
+
+print(alpha)
+ugol = 6
+print(ugol / sector)
+print(round(0.5))
+index = round(ugol / sector)
+print(index)
+print(alpha[index])
